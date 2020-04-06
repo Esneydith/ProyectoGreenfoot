73cc464bfe808d06d37ebe7246dd28467f11c36b
elperisco <55815694+elperisco@users.noreply.github.com>
2020-04-06 18:40:49 -0500

    Add files via upload

---
+++ TerranDerecha.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TerranIzquierdo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TerranDerecha extends Personajes
+{
+    /**
+     * Act - do whatever the TerranIzquierdo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
