01792ff5569ba2cad54577d3d07bcf3e18d96195
Esneydith <56083830+Esneydith@users.noreply.github.com>
2020-04-07 16:14:56 -0500

    Add files via upload

---
+++ Texto2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Texto2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Texto2 extends Textos
+{
+    /**
+     * Act - do whatever the Texto2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
